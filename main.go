a055f636c2ef7a5f429b0c069702a7836ccb7115
xagero <xagero@gmail.com>
2024-03-14 21:24:38 +0300

    Bump tag version
    (cherry picked from commit 8badb6a60d3627bf54dfbfa9a7533aa6837602c0)

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	fmt.Println(helper.IsEmpty(""))
 	fmt.Println(helper.IsNotEmpty(" "))
+	fmt.Println(helper.IsNotEmpty("Hello"))
 
 	fmt.Println(helper.IsBlank(""))
-	fmt.Println(helper.IsNotBlank(""))
+	fmt.Println(helper.IsNotBlank(" "))
+	fmt.Println(helper.IsNotBlank("Hello"))
+
 }
