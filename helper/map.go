7dcf8471ba41c508c8a82fecebab707e8db93db2
xagero <xagero@gmail.com>
2024-03-26 19:50:05 +0300

    add FetchValues function

--- helper/map.go
+++ helper/map.go
@@ -1,11 +1,21 @@
 package helper
 
-// FetchKeys return extracted keys
+// FetchKeys return extracted keys in random order
 func FetchKeys[key comparable, value any](input map[key]value) []key {
 	data := make([]key, 0, len(input))
 
 	for k := range input {
 		data = append(data, k)
+	}
+	return data
+}
+
+// FetchValues return extracted values in random order
+func FetchValues[key comparable, value any](input map[key]value) []value {
+	data := make([]value, 0, len(input))
+
+	for _, v := range input {
+		data = append(data, v)
 	}
 	return data
 }
--- test/map_test.go
+++ test/map_test.go
@@ -1,31 +1,52 @@
 package test
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/xagero/go-helper/helper"
 	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/xagero/go-helper/helper"
+)
+
+var (
+	expectedKeys   = []string{"red", "orange", "yellow", "green", "blue", "indigo", "violet"}
+	expectedValues = []string{
+		"I am red color!",
+		"I am orange color!",
+		"I am yellow color!",
+		"I am green color!",
+		"I am blue color!",
+		"I am indigo color!",
+		"I am violet color!",
+	}
+	actualMapTest = map[string]string{
+		"red":    "I am red color!",
+		"orange": "I am orange color!",
+		"yellow": "I am yellow color!",
+		"green":  "I am green color!",
+		"blue":   "I am blue color!",
+		"indigo": "I am indigo color!",
+		"violet": "I am violet color!",
+	}
 )
 
 func TestFetchKeys(t *testing.T) {
 
-	colors := []string{"red", "orange", "yellow", "green", "blue", "indigo", "violet"}
-
-	rainbow := make(map[string]string)
-
-	rainbow["red"] = "I am red color!"
-	rainbow["orange"] = "I am orange color!"
-	rainbow["yellow"] = "I am yellow color!"
-	rainbow["green"] = "I am green color!"
-	rainbow["blue"] = "I am blue color!"
-	rainbow["indigo"] = "I am indigo color!"
-	rainbow["violet"] = "I am violet color!"
-
-	keys := helper.FetchKeys(rainbow)
+	actual := helper.FetchKeys(actualMapTest)
 
 	// Sort data to ensure ordered position
-	sort.Strings(colors)
-	sort.Strings(keys)
+	sort.Strings(expectedKeys)
+	sort.Strings(actual)
 
-	assert.Equal(t, colors, keys)
+	assert.Equal(t, expectedKeys, actual)
+}
+
+func TestFetchValues(t *testing.T) {
+	actual := helper.FetchValues(actualMapTest)
+
+	// Sort data to ensure ordered position
+	sort.Strings(expectedValues)
+	sort.Strings(actual)
+
+	assert.Equal(t, expectedValues, actual)
 }
